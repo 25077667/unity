da9cb71cf3ca162a3517335095bff09832eeae16
25077667 <zxc25077667@gmail.com>
2018-05-30 13:18:49 +0800

    1

---
+++ test/Assets/NewBehaviourScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+    float coffee_tempeture = 87.0f;
+    float hot_lim = 60.0f;
+    float cold_lim = 10.0f;
+
+    void Awake()
+    {
+        Debug.Log("Awake called.");
+    }
+    // Use this for initialization
+    void Start () {
+        Debug.Log("HELLO");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Space))
+        {
+            test();
+
+        }
+	}
+    void test()
+    {
+        if()
+    }
+}
